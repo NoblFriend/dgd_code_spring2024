7863735bb587e2b27a6df5bf6f0f9511d40f56c8
Mikhail <kolosov.ma@phystech.edu>
2024-04-22 23:50:00 +0300

    Add ttsvd and tucker classes

--- cifar/utils/operators.py
+++ cifar/utils/operators.py
@@ -188,3 +188,81 @@ class HOSVD(CompOp):
             self.data_size += sv.numel()
 
         return reconstructed
+    
+class TTSVD(CompOp):
+    def __init__(self, max_rank = 25, target_dim = 32):
+        self.target_dim = target_dim
+        self.max_rank = max_rank
+
+    def _make_dims(self, shape, target_dim):
+        new_shape = []
+        for dim in shape:
+            while dim > target_dim:
+                if dim % target_dim == 0:
+                    new_shape.append(target_dim)
+                    dim //= target_dim
+                else:
+                    break
+            new_shape.append(dim)
+        return new_shape
+
+
+    def __call__(self, tensor: torch.Tensor):
+        shape = tensor.shape
+        new_shape = self._make_dims(shape, self.target_dim)
+
+        significant_dims = [dim for dim in new_shape if dim > 1]
+        if len(significant_dims) < 2:
+            return tensor
+
+        tensor = tensor.reshape(new_shape)
+        ranks = [min(self.max_rank, s) for s in new_shape] + [1]
+        ranks[0] = 1
+        
+        tt_tensor = tensor_train(tensor, rank=ranks)
+        ranks = tt_tensor.rank
+        dimensions = tt_tensor.shape
+        self.data_size = sum(ranks[i-1] * dimensions[i-1] * ranks[i] for i in range(1, len(ranks)))
+
+        reconstructed_tensor = tl.tt_to_tensor(tt_tensor)
+        reconstructed_tensor = reconstructed_tensor.reshape(shape)
+        return reconstructed_tensor
+    
+
+class TuckerOp(CompOp):
+    def __init__(self, max_rank = 25, target_dim = 32):
+        self.target_dim = target_dim
+        self.max_rank = max_rank
+
+    def _make_dims(self, shape, target_dim):
+        new_shape = []
+        for dim in shape:
+            while dim > target_dim:
+                if dim % target_dim == 0:
+                    new_shape.append(target_dim)
+                    dim //= target_dim
+                else:
+                    break
+            new_shape.append(dim)
+        return new_shape
+
+
+    def __call__(self, tensor: torch.Tensor):
+        shape = tensor.shape
+        new_shape = self._make_dims(shape, self.target_dim)
+
+        significant_dims = [dim for dim in new_shape if dim > 1]
+        if len(significant_dims) < 2:
+            return tensor
+
+        tensor = tensor.reshape(new_shape)
+        ranks = [min(25, s) for s in new_shape]
+        
+        core, factors = tucker(tensor, rank=list(ranks))
+        self.data_size = core.numel()
+        for sv in factors:
+            self.data_size += sv.numel()
+
+        reconstructed_tensor = tl.tucker_to_tensor((core, factors))
+        reconstructed_tensor = reconstructed_tensor.reshape(shape)
+        return reconstructed_tensor
